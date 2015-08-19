46c6f366468b644959312633a0993e8c209a0c94
EmmaKong <1229480203@qq.com>
2015-08-19 15:01:52 +0800

    .

--- AutoRunner/iMusicOperation.java
+++ AutoRunner/iMusicOperation.java
@@ -16,17 +16,28 @@ public class iMusicOperation extends UiAutomatorTestCase {
 		UiObject nosongsIndicate = new UiObject(new UiSelector().resourceId("com.android.bbkmusic:id/nosongtext"));
 		if(nosongsIndicate.exists()){
 			
+			System.out.println("There is no songs in this phone");
 			SearchandDownloadSong("See You Again");
-			sleep(2000);
-			
-			
+			sleep(1000);					
 			SearchandDownloadSong("mygod");
+			sleep(1000);
+			SearchandDownloadSong("Better");
+			sleep(1000);
+			SearchandDownloadSong("You Raise Me Up");
+			sleep(1000);
+	    	SearchandDownloadSong("haha");
+					
+		}else{
 			
-			
-			
+			 SearchandPlaySong("hha");
+			 sleep(1000);
+			 SearchandPlaySong("Better");
+			 
+			 SearchandDownloadSong("Nothing");
+			 
+			 PlaySongRandomly(); 
 			
 		}
-		
 		
 		
 		
@@ -59,45 +70,73 @@ public class iMusicOperation extends UiAutomatorTestCase {
 		searchView.click();
 		
 		searchView.setText(songname);
+		sleep(2000);
 		
-        UiObject thesong = new UiObject(new UiSelector().text(songname)); 
-        
+		UiObject searchlistView = new UiObject(new UiSelector().resourceId("com.android.bbkmusic:id/search_list_view"));	
+		UiObject songlayout = searchlistView.getChild(new UiSelector().className("android.widget.FrameLayout"));
+		
+		UiObject thesong = songlayout.getChild(new UiSelector().text(songname));  
+		
         if(thesong.exists()){
         	System.out.println("Find the song: " + songname);
         	//使用getFromParent()方法 获取当前对象同级的 演唱者名字
-        	UiObject aristname = thesong.getFromParent(new UiSelector().resourceId("com.android.bbkmusic:id/arist_name"));
+        	String aristname = thesong.getFromParent(new UiSelector().resourceId("com.android.bbkmusic:id/arist_name")).getText();       	
         	System.out.println("The arist is: " + aristname);
-        	thesong.clickAndWaitForNewWindow();
+        	     	
+        	UiObject downloadButton = songlayout.getChild(new UiSelector().resourceId("com.android.bbkmusic:id/more_layout"));
         	
-        	
-        	
-        	
-        	
+        	downloadButton.click();
+        	sleep(2000);
+ 
         	UiObject cancelButton = new UiObject(new UiSelector().text("Cancel"));	
         	cancelButton.click();
         	
         	
         }else{
-        	
-        	UiObject listView = new UiObject(new UiSelector().resourceId("com.android.bbkmusic:id/search_list_view"));
-        	UiObject onesong = listView.getChild(new UiSelector().index(0));
+        	// 取搜索列表中的第一项       	
+        	UiObject onesong = searchlistView.getChild(new UiSelector().index(0));
+        	String onesongname = onesong.getChild(new UiSelector().resourceId("com.android.bbkmusic:id/music_name")).getText();
+        	System.out.println("Download a song: " + onesongname);
         	UiObject downloadbutton = onesong.getChild(new UiSelector().resourceId("com.android.bbkmusic:id/more_layout"));
         	downloadbutton.click();
         	
-       
+            sleep(2000);
         	UiObject cancelButton = new UiObject(new UiSelector().text("Cancel"));	
-        	cancelButton.click();
-        	
-        	
-        	
-        }
+        	cancelButton.click();        	
+        }	
 		
+	}
+	
+	private void PlaySongRandomly() throws UiObjectNotFoundException{  //随机播放
+		UiObject iMusictabs = new UiObject(new UiSelector().resourceId("android:id/tabs"));
+		UiObject Songs = iMusictabs.getChild(new UiSelector().text("Songs").resourceId("android:id/title"));
+		Songs.clickAndWaitForNewWindow();
 		
+		UiObject shuffle = new UiObject(new UiSelector().resourceId("com.android.bbkmusic:id/shuffer_text"));
+		shuffle.click();
+		
+		getUiDevice().pressBack();
+	
+	}
+	
+	private void SearchandPlaySong(String songname) throws UiObjectNotFoundException{
+		
+		UiObject iMusictabs = new UiObject(new UiSelector().resourceId("android:id/tabs"));
+		UiObject Songs = iMusictabs.getChild(new UiSelector().text("Songs").resourceId("android:id/title"));
+		Songs.clickAndWaitForNewWindow();
+		
+		UiObject wantedsong = new UiObject(new UiSelector().text(songname));
+		if(wantedsong.exists()){
+			System.out.println("Find the song: " + songname);
+			wantedsong.click();
+			getUiDevice().pressBack();
+		}else{
+			System.out.println("Cannot find the song: " + songname);
+		}
 		
 		
 		
 	}
-	
 	
 
 }
